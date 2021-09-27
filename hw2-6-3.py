33c6462bbd9482becce5da09f65d623cf6d1c342
P22inolan <p22inolan@fairfieldprep.org>
2021-09-27 10:18:49 -0400

    Small space change

--- hw2-6-3.py
+++ hw2-6-3.py
@@ -1,7 +1,7 @@
 # Author: IBN (AMDG) 9/20/2021
-free_throws = input("How many free throws were scored?")
-two_points = input("How many two pointers were scored?")
-three_points = input("How many three pointers were scored?")
+free_throws = input("How many free throws were scored? ")
+two_points = input("How many two pointers were scored? ")
+three_points = input("How many three pointers were scored? ")
 
 score = (int(free_throws) + (int(two_points) * 2) + (int(three_points) * 3))
 
